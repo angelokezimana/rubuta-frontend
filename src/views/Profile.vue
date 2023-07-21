ef5ea38d7a6b787fd6f65557251b8ee4b0fd98bd
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-07-21 16:12:20 +0200

    feat: add profile file

---
+++ src/views/Profile.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { onMounted } from 'vue';
+import { useAuthStore } from '../store'
+
+const authStore = useAuthStore()
+onMounted(async()=>{
+    await authStore.getUser()
+})
+</script>
+
+<template>
+    <div v-if="authStore.currentUser">
+        <h1>Profile</h1>
+        <p>Name: {{ authStore.currentUser.last_name }} {{ authStore.currentUser.first_name }}</p>
+        <p>Email: {{ authStore.currentUser.email }}</p>
+    </div>
+</template>
