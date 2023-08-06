d5282f1b826c7d04cb14c4c37459ec47d4ad8278
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-08-06 17:59:17 +0200

    chore: rename Profile.vue to UserProfile.vue

---
+++ src/views/UserProfile.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useAuthStore } from '../store'
+
+const authStore = useAuthStore()
+</script>
+
+<template>
+    <div v-if="authStore.currentUser">
+        <h1>Profile</h1>
+        <p>
+            Name: {{ authStore.currentUser.last_name }}
+            {{ authStore.currentUser.first_name }}
+        </p>
+        <p>Email: {{ authStore.currentUser.email }}</p>
+    </div>
+</template>
