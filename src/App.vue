d7e92dc733558dc0478eceba3054554a325bd6f1
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-08-06 18:05:25 +0200

    chore: remove tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -5,139 +5,48 @@ const authStore = useAuthStore()
 </script>
 
 <template>
-    <nav
-        v-if="authStore.isAuthenticated"
-        class="relative flex w-full flex-nowrap items-center justify-between bg-[#FBFBFB] py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:flex-wrap lg:justify-start lg:py-4"
-        data-te-navbar-ref
-    >
-        <div class="flex w-full flex-wrap items-center justify-between px-3">
-            <div class="ml-2">
-                <a
-                    class="text-xl text-neutral-800 dark:text-neutral-200"
-                    href="#"
-                    >RUBUTA</a
-                >
-            </div>
-            <!-- Hamburger button for mobile view -->
-            <button
-                class="block border-0 bg-transparent px-2 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
-                type="button"
-                data-te-collapse-init
-                data-te-target="#navbarSupportedContent3"
-                aria-controls="navbarSupportedContent3"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <!-- Hamburger icon -->
-                <span class="[&>svg]:w-7">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                        class="h-7 w-7"
-                    >
-                        <path
-                            fill-rule="evenodd"
-                            d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                            clip-rule="evenodd"
-                        />
-                    </svg>
-                </span>
-            </button>
+    <nav>
+        <div>
+            <router-link :to="{ name: 'Home' }">RUBUTA</router-link>
+        </div>
 
-            <!-- Collapsible navbar container -->
-            <div
-                id="navbarSupportedContent3"
-                class="!visible mt-2 hidden flex-grow basis-[100%] items-center lg:mt-0 lg:!flex lg:basis-auto"
-                data-te-collapse-item
-            >
-                <!-- Left links -->
-                <ul
-                    class="list-style-none mr-auto flex flex-col pl-0 lg:mt-1 lg:flex-row"
-                    data-te-navbar-nav-ref
-                >
-                    <!-- New Message link -->
-                    <li
-                        class="my-4 pl-2 lg:my-0 lg:pl-2 lg:pr-1"
-                        data-te-nav-item-ref
-                    >
-                        <router-link
-                            class="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                            aria-current="page"
-                            to="/profile"
-                            data-te-nav-link-ref
-                            >New Message</router-link
-                        >
-                    </li>
-                    <!-- Inbox link -->
-                    <li
-                        class="mb-4 pl-2 lg:mb-0 lg:pl-0 lg:pr-1"
-                        data-te-nav-item-ref
-                    >
-                        <router-link
-                            class="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                            to="/profile"
-                            data-te-nav-link-ref
-                            >Inbox</router-link
-                        >
-                    </li>
-                    <!-- Sent link -->
-                    <li
-                        class="mb-4 pl-2 lg:mb-0 lg:pl-0 lg:pr-1"
-                        data-te-nav-item-ref
-                    >
-                        <router-link
-                            class="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                            to="/profile"
-                            data-te-nav-link-ref
-                            >Sent</router-link
-                        >
-                    </li>
-                    <!-- Sent link -->
-                    <li
-                        class="mb-4 pl-2 lg:mb-0 lg:pl-0 lg:pr-1"
-                        data-te-nav-item-ref
-                    >
-                        <router-link
-                            class="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                            to="/profile"
-                            data-te-nav-link-ref
-                            >Draft</router-link
-                        >
-                    </li>
-                    <!-- Sent link -->
-                    <li
-                        class="mb-4 pl-2 lg:mb-0 lg:pl-0 lg:pr-1"
-                        data-te-nav-item-ref
-                    >
-                        <router-link
-                            class="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                            to="/profile"
-                            data-te-nav-link-ref
-                            >Bin</router-link
-                        >
-                    </li>
-                </ul>
-                <span class="ml-2 text-neutral-500 dark:text-neutral-200">
-                    Hello {{ authStore.user.last_name }} |
-                    <router-link
-                        :to="{ name: 'Profile' }"
-                        class="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                        >Profile</router-link
-                    >
-                    |
-                    <button
-                        type="button"
-                        class="p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-                        @click="authStore.logout"
-                    >
-                        Logout
-                    </button></span
-                >
-            </div>
+        <div v-if="authStore.currentUser">
+            <ul>
+                <!-- New Message link -->
+                <li>
+                    <router-link to="/profile">New Message</router-link>
+                </li>
+                <!-- Inbox link -->
+                <li>
+                    <router-link to="/profile">Inbox</router-link>
+                </li>
+                <!-- Sent link -->
+                <li>
+                    <router-link to="/profile">Sent</router-link>
+                </li>
+                <!-- Sent link -->
+                <li>
+                    <router-link to="/profile">Draft</router-link>
+                </li>
+                <!-- Sent link -->
+                <li>
+                    <router-link to="/profile">Bin</router-link>
+                </li>
+            </ul>
+            <span>
+                Hello {{ authStore.currentUser.last_name }} |
+                <router-link :to="{ name: 'Profile' }">Profile</router-link>
+                |
+                <button type="button" @click="authStore.logout">Logout</button>
+            </span>
+        </div>
+        <div v-else>
+            <router-link :to="{ name: 'Login' }">Log in</router-link>
+            |
+            <router-link :to="{ name: 'SignUp' }">Sign up</router-link>
         </div>
     </nav>
-    <div class="p-4">
+    <div>
         <router-view />
     </div>
 </template>
